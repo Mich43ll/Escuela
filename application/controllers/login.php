64925561bfd6004f7672e1d73f371bebe8ecb8e2
Mich43ll <mosoriobarahona@gmail.com>
2022-12-01 15:09:52 -0600

    login completo

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -9,9 +9,13 @@ class login extends CI_Controller
 
     public function index()
     {
+        if($this->session->userdata('username')){
+            redirect('Inicio');
+        }
         if (isset($_POST['password'])) {
             $this->load->model('login_model');
             if($this->login_model->login($_POST['username'],$_POST['password'])){
+                $this->session->set_userdata('username',$_POST['username']);
                 redirect('Inicio');
             }else{
                 redirect('login');
@@ -22,5 +26,9 @@ class login extends CI_Controller
             $this->load->view('login/inicio');
             $this->load->view('plantilla/scripts');
             $this->load->view('plantilla/end');
+    }
+    public function logout(){
+        $this->session->sess_destroy();
+        redirect('login');
     }
 }
