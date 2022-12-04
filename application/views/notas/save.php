8c20b5bdf8b6a1c8b9397aff862809dc50a5fb8d
Mich43ll <mosoriobarahona@gmail.com>
2022-12-03 23:23:40 -0600

    validaciones terminadas

--- application/views/notas/save.php
+++ application/views/notas/save.php
@@ -3,39 +3,39 @@
 <h1 style= text-align:center>Ingresar notas</h1>
 <form action="<?php anchor("notas/save") ?>"  method ="POST" autocomplete="off">
   <div class="inset">
-
+  <div style="color:#a94442;background-color:#f2dede;border-color:#ebccd1; border-radius:5px"><?php echo validation_errors();?></div>
   <p>
     <label for="text">Primer Parcial</label>
-    <input type="text" name="primerParcial" >
+    <input type="text" name="primerParcial" required>
   </p>
 
   <p>
     <label for="text">Segundo Parcial</label>
-    <input type="text" name="segundoParcial">
+    <input type="text" name="segundoParcial" required>
   </p>
 
   <p>
     <label for="text">Tercer Parcial</label>
-    <input type="text" name="tercerParcial" >
+    <input type="text" name="tercerParcial" required>
   </p>
   
   <p>
     <label for="text">idClase</label>
-    <input type="text" name="idClase">
+    <input type="text" name="idClase" required>
   </p>
   <p>
     <label for="text">idAlumno</label>
-    <input type="text" name="idAlumno">
+    <input type="text" name="idAlumno" required>
   </p>
   <p>
     <label for="text">idEmpleado</label>
-    <input type="text" name="idEmpleado">
+    <input type="text" name="idEmpleado" required>
   </p>
 
   
   <button type="submit" class="btn btn-primary">Guardar</button>
   
-    <?php echo anchor('/inicio','Cancelar','class="btn btn-danger"');?>
+    <?php echo anchor('notas/show','Cancelar','class="btn btn-danger"');?>
  
   </div>
 
